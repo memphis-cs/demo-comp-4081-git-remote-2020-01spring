0494f3f78ed4e3dcac6df68b9edbd7b714ed556b
Scott Fleming <sdflem@gmail.com>
2020-04-13 21:24:22 -0500

    Seed cars database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Car.create!(
+  make: 'Oldsmobile',
+  model: 'Cutlass',
+  year: 1980
+)
+
+Car.create!(
+  make: 'Chevrolet',
+  model: 'Chevette',
+  year: 1981
+)
+
+Car.create!(
+  make: 'Ford',
+  model: 'F-Series',
+  year: 1982
+)
+
+Car.create!(
+  make: 'Chrysler',
+  model: 'Minivan',
+  year: 1984
+)
+
+Car.create!(
+  make: 'Chevrolet',
+  model: 'Celebrity',
+  year: 1986
+)
+
+Car.create!(
+  make: 'Ford',
+  model: 'Escort',
+  year: 1988
+)
